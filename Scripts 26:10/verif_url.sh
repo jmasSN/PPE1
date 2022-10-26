c8d903cbf4926a655c14b06f2c5b6941b5507ab0
jmasSN <juliette.massy@sorbonne-nouvelle.fr>
2022-10-26 10:29:36 +0200

    Ajout script URL de la séance du 26/10

---
+++ Scripts 26:10/verif_url.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+if [ $# -ne 1] # Si le nb d'argument est différent de 1 alors : (avec ne = not equal)
+then
+	echo "Ce programme demande un fichier en argument."
+		exit
+	fi
+	
+	if [ -f $1 ] # Si l'argument est un fichier qui existe alors :
+	then
+		echo "Fichier OK"
+	else
+		echo "Ceci n'est pas un fichier"
+		exit
+	FICHIER_URLS=$1
+	OK=0
+	NOK=0
+	while read -r LINE ; # -r => ignore les échappements qui ne sont pas encodés (genre \n)
+	do
+		echo "La ligne : $LINE"
+		if [[ $LINE =~ "https?://"]]
+		then
+			echo "ressemble à une URL valide"
+			OK=$(expr $OK + 1)
+		else
+			echo "ne ressemble pas à une URL valide."
+			NOK=$(expr $NOK + 1)
+		fi
+	done <	FICHIER_URLS
+	echo "$OK URLs et $NOK lignes douteuses."
