94dba7ef6f064c5ffc11bdb6e4817943d3a323b6
jmasSN <juliette.massy@sorbonne-nouvelle.fr>
2022-11-09 12:14:22 +0100

    hcjkdsh

--- traitement_url_base.sh
+++ traitement_url_base.sh
@@ -14,15 +14,33 @@ fichier_tableau=$2 # le fichier HTML en sortie
 
 # !!!!!!
 # ici on doit vérifier que nos deux paramètres existent, sinon on ferme!
-# !!!!!!
+
+# Si le nb d'argument est différent de 2 alors : (avec ne = not equal)
+if [ $# -ne 2 ] 
+then
+	echo "Ce programme demande deux fichiers en argument."
+	exit
+fi
+
+if [ -f $1 ] # Si l'argument est un fichier qui existe alors :
+	then
+		echo "Fichier OK"
+	else
+		echo "Ceci n'est pas un fichier"
+		exit
+fi
 
 # modifier la ligne suivante pour créer effectivement du HTML
 echo "Je dois devenir du code HTML à partir de la question 3" > $fichier_tableau
 
 lineno=1;
 
+
 while read -r line;
 do
 	echo "ligne $lineno: $line";
-	lineno=$((lineno+1));
+	entete_serveur=$(curl -I -s $line) #-s pour éviter les infos qui nous intéressent pas
+	code_erreur=$($entete_serveur
+	echo "$entete_serveur"
+	lineno=$((lineno+1))
 done < $fichier_urls
