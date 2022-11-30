a4af2f25a755c5e199c84799f19d0c34e6f59763
jmasSN <juliette.massy@sorbonne-nouvelle.fr>
2022-11-30 09:48:21 +0100

    Aspirations tests

--- scriptaspi.sh
+++ scriptaspi.sh
@@ -1,58 +1,79 @@
 #!/usr/bin/env bash
 
 #===============================================================================
-# VOUS DEVEZ MODIFIER CE BLOC DE COMMENTAIRES.
-# Ici, on décrit le comportement du programme.
-# Indiquez, entre autres, comment on lance le programme et quels sont
+# Ce script est notre programme pour traiter les URLs. Il permet de créer un tableau HTML en sortie avec chaque URL, le code de réponse correspondant .
+# Il s'utilise dans le dossier avec le fichier à traiter <URL.txt> (avec un URL/ligne) avec la ligne de commande :
+# bash traitement_url_base.sh <URL.txt>  <tableauURL.html> 
+
+#<tableauURL.html>  est le fichier de sortie du tableau HTML (ici il n'existe pas avant le lancement du script).
+#Il est nécéssaire d'avoir créer un dossier 'aspirations' dans le dossier d'utilisation du script.
+#===============================================================================
+
+# On vérifie que l'on a 2 arguments :
 if [ $# -ne 2 ]
 then
 	echo " ce programme demande deux arguments"
 	exit
 fi
-# les paramètres.
-# La forme est indicative, sentez-vous libres d'en changer !
-# Notamment pour quelque chose de plus léger, il n'y a pas de norme en bash.
-#===============================================================================
 
+# On vérifie que le fichier d'URLs existe :
+if [ -f $1 ]
+	then
+		echo "Fichier OK"
+	else
+		echo "Ceci n'est pas un fichier valide."
+		exit
+fi
+
+
+# On attribue les fichiers en argument à des variables :
 fichier_urls=$1 # le fichier d'URL en entrée
 fichier_tableau=$2 # le fichier HTML en sortie
-# !!!!!!
-# ici on doit vérifier que nos deux paramètres existent, sinon on ferme!
-# !!!!!!
 
 # modifier la ligne suivante pour créer effectivement du HTML
 
 palabra="étranger" # on peut faire mieux ez
 echo $fichier_urls;
 basename=$(basename -s .txt $fichier_urls)
-   
-echo "<html><body>" > $fichier_tableau
+
+# On crée l'architecture HTML du tableau de sortie dans le fichier donné en second argument :
+echo "<html><head><meta charset="UTF-8" /><link rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"/>" > $fichier_tableau
+echo "<body>" > $fichier_tableau
 echo "<h2>Tableau $basename :</h2>" >> $fichier_tableau
 echo "<br/>" >> $fichier_tableau
 echo "<table>" >> $fichier_tableau
 echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th></tr>" >> $fichier_tableau
 
+# On crée un compteur pour les URLs/le nombre de lignes :               
 lineno=1;
-                while read -r line;
+# On lit chaque ligne :
+while read -r line;
 do
-        URL=$line
-        CODEHTTP=$(curl -I -s $line | grep -e "^HTTP/" | grep -Eo "[0-9]{3}" | head -n1)	
-        ENC=$(curl -I -s $line | grep -Po "charset=[\w-]+"| cut -d= -f2)
-		aspiration=$(curl $URL)
+# On récupère l'URL (=la ligne)
+    URL=$line
+	# On récupère le code réponse
+    CODEHTTP=$(curl -I -s $line | grep -e "^HTTP/" | grep -Eo "[0-9]{3}" | head -n1)	
+	# On récupère l'encodage
+	ENC=$(curl -I -s $line | grep -Po "charset=[\w-]+"| cut -d= -f2)
+	# On récupère le contenu de la page HTML
+	aspiration=$(curl $URL)
+	# On met chaque aspirations dans un fichier nouveau fichier HTML
 	echo "$aspiration" > "aspirations/$basename-$lineno.html"
 
+	# On affiche les informations dans le terminal
 	#echo -e "\tURL : $URL";
 	echo -e "\tURL N°: $lineno";
     echo -e "\tcode : $CODEHTTP";
-
+	
 	if [[ ! $ENC ]]
 	then
-		echo -e "\tencodage non détecté, on prendra UTF-8 par défaut.";
+		echo -e "\tEncodage non détecté, on prendra UTF-8 par défaut.";
 		ENC="UTF-8";
 	else
-		echo -e "\tencodage : $ENC";
+		echo -e "\tEncodage : $ENC";
 	fi
-
+	# Si le code der éponse est différent de 200
 	if [[ $CODEHTTP -eq 200 ]]                                                                                                                                         
 	then
 		dump=$(lynx -dump -nolist -assume_charset=$ENC -display_charset=$ENC $URL)
